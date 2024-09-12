cd1aa3d283366cc92b65d8229c51908707bb2a76
TharHtet236401 <tharhteta3@gmail.com>
2024-09-12 21:20:19 +0700

    mongodb configured

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 import express from "express";
 import TelegramBot from "node-telegram-bot-api";
+import mongoose from "mongoose";
+
+import { connectDB } from "./config/connectMongo.js";
 
 dotenv.config();
 
@@ -8,7 +11,17 @@ const app = express();
 
 app.use(express.json());
 
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+app.use(express.static("public"));
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const message = msg.text;
+
+  bot.sendMessage(chatId, `You said: ${message}`);
+});
+
+app.listen(process.env.PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
