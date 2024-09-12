d0d99ef22d8cd4b87d93bc38cea619b3c91948b8
TharHtet236401 <tharhteta3@gmail.com>
2024-09-12 21:00:14 +0700

    first commit

---
+++ models/food.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const foodSchema = new mongoose.Schema({
+    foodName: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+});
---
+++ server.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import express from "express";
+import TelegramBot from "node-telegram-bot-api";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
